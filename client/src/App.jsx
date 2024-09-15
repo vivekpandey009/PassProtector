1984e058b820e6edf38202088e025dc681aac122
Vivek Pandey <vivek.vidyamandir@gmail.com>
2024-09-15 14:27:07 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/signin";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div>
-      <h1 className="text-red-400">App</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
