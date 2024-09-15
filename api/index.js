0717b4c16df61c52d456781fc848e74253b9b871
Vivek Pandey <vivek.vidyamandir@gmail.com>
2024-09-15 16:43:09 +0530

    Conneted to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connectde to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
