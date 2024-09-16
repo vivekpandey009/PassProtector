6e478625c5d40819d56e6063659f0b94fc30e29a
Vivek Pandey <vivek.vidyamandir@gmail.com>
2024-09-16 20:21:11 +0530

    Create Signup API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -13,6 +15,12 @@ mongoose
     console.log(err);
   });
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
