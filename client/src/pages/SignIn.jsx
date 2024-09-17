6dedb487f49b1fc50f1cf4df86b22d979a80c49d
Vivek Pandey <vivek.vidyamandir@gmail.com>
2024-09-17 22:45:07 +0530

    implementing Redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -13,24 +20,28 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    setLoading(true);
-    setError(false);
+    try {
+      dispatch(signInStart());
 
-    const res = await fetch("/api/auth/signin", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
 
-    setLoading(false);
-    if (data.success === false) {
-      setError(true);
-      return;
+      dispatch(signInSuccess(data));
+
+      if (data.success === false) {
+        dispatch(signInFailure(data));
+        return;
+      }
+      navigate("/");
+    } catch (error) {
+      dispatch(signInFailure(error));
     }
-    navigate("/");
   };
 
   return (
@@ -69,7 +80,7 @@ export default function SignIn() {
       </div>
 
       <p className="p-2 m-3 text-xl text-center text-red-600 rounded-lg">
-        {error && "Something went wrong!"}
+        {error ? error.message || "Something went wrong!" : ""}
       </p>
     </div>
   );
